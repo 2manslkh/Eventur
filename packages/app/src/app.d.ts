278ef94e636dacee72c1bbd86e6a343664759f92
Kenk <kenghin_lim@hotmail.com>
2024-08-03 17:49:07 +0700

    add types

--- packages/app/src/app.d.ts
+++ packages/app/src/app.d.ts
@@ -2,6 +2,29 @@
 // for information about these interfaces
 declare global {
   namespace App {
+    interface Locals {
+      /**
+       * The last frame signature packet received from a Farcaster client as a result of the user clicking a frame button.
+       */
+      frameSignaturePacket?: FrameSignaturePacket;
+    }
+
+    interface PageData {
+      /**
+       * The title of the page that is being rendered as a frame.
+       */
+      title?: string;
+
+      /**
+       * The width of the frame image in pixels.
+       */
+      width?: number;
+
+      /**
+       * Farcaster frame metadata associated with this page, including image aspect ratio and buttons to render when this page's URL is embedded in a Farcaster cast.
+       */
+      frame?: Omit<FrameMeta, 'image'> & { image?: Omit<FrameMeta['image'], 'url'> & { url?: string } };
+    }
     // interface Error {}
     // interface Locals {}
     // interface PageData {}
--- packages/app/src/types/types.ts
+++ packages/app/src/types/types.ts
@@ -8,3 +8,21 @@ export interface Toast {
 }
 
 export type ScreenMode = 'desktop' | 'mobile';
+
+export interface Event {
+  id: number;
+  name: string;
+  date: string;
+  location: string;
+  description: string;
+  capacity: number;
+}
+
+export type RSVPStatus = 'yes' | 'no' | 'pending';
+
+export interface Attendee {
+  id: number;
+  address: string;
+  rsvpStatus: RSVPStatus;
+  eventId: number;
+}
